24e076a2084eb0f12f392a02e448cd9f14ae8d0b
Kwhildne <kwhildne@ncsu.edu>
2017-10-04 12:36:02 -0400

    Added more tests to CourseNameValidatorFSMTest.java

--- PackScheduler/test/validator/CourseNameValidatorFSMTest.java
+++ PackScheduler/test/validator/CourseNameValidatorFSMTest.java
@@ -6,8 +6,8 @@ import edu.ncsu.csc216.pack_scheduler.course.validator.CourseNameValidatorFSM;
 import edu.ncsu.csc216.pack_scheduler.course.validator.InvalidTransitionException;
 
 /**
- * Tests CourseNameValidatorFSM's ability to transition out from each of its states, and to 
- * validate a proper course name
+ * Tests CourseNameValidatorFSM's ability to transition out from each of its
+ * states, and to validate a proper course name
  * 
  * @author Kevin Hildner
  */
@@ -16,7 +16,7 @@ public class CourseNameValidatorFSMTest {
 	public static final String INVALID_COURSE_NAME_1 = "CSC!16";
 	/** Invalid course name due to starting with a digit */
 	public static final String INVALID_COURSE_NAME_2 = "2CSC16";
-	
+
 	/** Valid course name with 1 letter, no suffix */
 	public static final String VALID_COURSE_NAME_L1 = "A216";
 	/** Valid course name with 2 letters, no suffix */
@@ -27,30 +27,85 @@ public class CourseNameValidatorFSMTest {
 	public static final String VALID_COURSE_NAME_L4 = "CSCB111";
 	/** Valid course name with 4 letters, and a suffix */
 	public static final String VALID_COURSE_NAME_L4_S = "CSCB123C";
-	
+
 	/**
 	 * 
 	 */
 	@Test
 	public void TestInitialStateTransitions() {
-		//Test 
+		// Construct a CourseNameValidatorFSM to be used throughout the tests
+		CourseNameValidatorFSM fsm = new CourseNameValidatorFSM();
+
+		// Test invalid course name starting with a digit
+		try {
+			fsm.isValid(INVALID_COURSE_NAME_2);
+			fail();
+		} catch (InvalidTransitionException e) {
+			assertEquals(e.getMessage(), "Course name must start with a letter.");
+		}
+
+		// Test valid course name starting with one digit, transition to stateL
+		try {
+			assertTrue(fsm.isValid(VALID_COURSE_NAME_L1));
+		} catch (InvalidTransitionException e) {
+			fail();
+		}
 	}
-	
+
+	/**
+	 * 
+	 */
+	@Test
+	public void TestStateLTransitions() {
+		// Construct a CourseNameValidatorFSM to be used throughout the tests
+		CourseNameValidatorFSM fsm = new CourseNameValidatorFSM();
+
+		// Test valid course name with 2 letters, transition to stateLL
+		try {
+			assertTrue(fsm.isValid(VALID_COURSE_NAME_L2));
+		} catch (InvalidTransitionException e) {
+			fail();
+		}
+
+		// Test valid course name with 1 letter, transition to stateD
+		try {
+			assertTrue(fsm.isValid(VALID_COURSE_NAME_L1));
+		} catch (InvalidTransitionException e) {
+			fail();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	@Test
+	public void TestStateLLTransitions() {
+		// Construct a CourseNameValidatorFSM to be used throughout the tests
+		CourseNameValidatorFSM fsm = new CourseNameValidatorFSM();
+		
+		// Test valid course name with 3 letters, transition to stateLLL
+		try {
+			assertTrue(fsm.isValid(VALID_COURSE_NAME_L3));
+		} catch (InvalidTransitionException e) {
+			fail();
+		}
+	}
+
 	/**
 	 * 
 	 */
 	@Test
 	public void TestInvalidTransitions() {
-		//Construct a CourseNameValidatorFSM to be used throughout the tests
+		// Construct a CourseNameValidatorFSM to be used throughout the tests
 		CourseNameValidatorFSM fsm = new CourseNameValidatorFSM();
-		
-		//Test input with an invalid character
+
+		// Test input with an invalid character
 		try {
 			fsm.isValid(INVALID_COURSE_NAME_1);
 			fail();
-		} catch(InvalidTransitionException e) {
+		} catch (InvalidTransitionException e) {
 			assertEquals(e.getMessage(), "Course name can only contain letters and digits.");
 		}
-		
+
 	}
 }
