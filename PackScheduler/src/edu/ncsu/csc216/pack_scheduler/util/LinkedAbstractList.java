04232fed323836c4e08ea2772e6cead0b94cf024
nnbenven <nnbenven@ncsu.edu>
2017-10-18 10:53:39 -0400

    Added method signatures for add, remove, get, set and size
    Pair programmed by Noah Benveniste and Kevin Hildner

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
@@ -2,18 +2,50 @@ package edu.ncsu.csc216.pack_scheduler.util;
 
 import java.util.AbstractList;
 
-public class LinkedAbstractList extends AbstractList {
+/**
+ * 
+ * @author Noah Benveniste
+ * @author Kevin Hildner
+ * @param <E> Indicates that the list can store any object type
+ */
+public class LinkedAbstractList<E> extends AbstractList<E> {
 
+	/**
+	 * 
+	 */
+	public void add(int idx, E element) {
+		
+	}
+	
+	/**
+	 * 
+	 */
+	public E remove(int idx) {
+		return null;
+	}
+	
+	/**
+	 * @return 
+	 */
+	public E set(int idx, E element) {
+		return null;
+	}
+	
+	/**
+	 * 
+	 */
 	@Override
-	public Object get(int arg0) {
+	public E get(int arg0) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	/**
+	 * 
+	 */
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
-
 }
