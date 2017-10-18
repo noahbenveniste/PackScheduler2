6ac0f2faa033a9b3d51f0fa949178c1836359838
nnbenven <nnbenven@ncsu.edu>
2017-10-18 11:44:42 -0400

    Implemented add()
    pair programmed by Noah Benveniste and Kevin Hildner

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
@@ -31,10 +31,48 @@ public class LinkedAbstractList<E> extends AbstractList<E> {
 	}
 	
 	/**
-	 * 
+	 * Adds an element to a specified index in the list
+	 * @throws IllegalArgumentException if the list is full or if a duplicate element is added
+	 * @throws NullPointerException if the added element is null
+	 * @throws IndexOutOfBoundsException if the index is less than zero or greater than the list's size
 	 */
 	public void add(int idx, E element) {
-		
+		//Check if the list is full
+		if (this.size() == this.capacity) {
+			throw new IllegalArgumentException("List is full.");
+		}
+		//Check for null input data
+		if (element == null) {
+			throw new NullPointerException("Cannot add null elements.");
+		}
+		//Check for duplicates
+		ListNode current = this.front;
+		for (int i = 0; i < this.size(); i++) {
+			if (current.data.equals(element)) {
+				throw new IllegalArgumentException("Cannot add duplicate elements.");
+			}
+			current = current.next;
+		}
+		//Check for out of bounds index
+		if (idx < 0 || idx > this.size()) {
+			throw new IndexOutOfBoundsException("Index is outside the acceptable range.");
+		}
+		//Adding to the front of the list
+		if (idx == 0) {
+			ListNode newFront = new ListNode(element, this.front); //Make the new node point to the old front
+			this.front = newFront; //Make the front field point to the new front;
+		}
+		//Adding to the middle or end of the list
+		current = this.front;
+		for (int i = 0; i < idx; i++) { //Traverse the list to get the reference to the old element at the index to add to
+			current = current.next;
+		}
+		ListNode newNode = new ListNode(element, current); //Create a new node that points to the old element at the index to add to
+		current = this.front;
+		for (int i = 0; i < idx - 1; i++) { //Traverse the list to get to the index just before the index to add to
+			current = current.next;
+		}
+		current.next = newNode; //Make the element at the index just before the index being added to point to the newly created node
 	}
 	
 	/**
