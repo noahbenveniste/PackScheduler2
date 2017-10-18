f483381a05978ff9501d487d4e128d3bdc759d89
nnbenven <nnbenven@ncsu.edu>
2017-10-18 10:46:49 -0400

    Created LinkedAbstractList

---
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/LinkedAbstractList.java
@@ -0,0 +1,19 @@
+package edu.ncsu.csc216.pack_scheduler.util;
+
+import java.util.AbstractList;
+
+public class LinkedAbstractList extends AbstractList {
+
+	@Override
+	public Object get(int arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
