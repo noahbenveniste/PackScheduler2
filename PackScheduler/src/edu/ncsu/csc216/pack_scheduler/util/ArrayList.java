21a7a2e22971102748d2889c111fb678e0c0d514
nnbenven <nnbenven@ncsu.edu>
2017-10-17 21:22:56 -0400

    Fixed bug in ArrayList
    -fixed bug when the underlying array was grown, not all values were
    being copied over to the new array
    
    debugged by Noah Benveniste and Kevin Hildner

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/util/ArrayList.java
@@ -82,7 +82,7 @@ public class ArrayList<E> extends AbstractList<E> {
 		//Cast to generic type
 		E[] temp = (E[]) o;
 		//Assign the elements from the old array to the same index in the new array
-		for (int i = 0; i < this.size() - 1; i++) {
+		for (int i = 0; i < this.size(); i++) {
 			temp[i] = this.list[i];
 		}
 		//Assign the new array to the list field
