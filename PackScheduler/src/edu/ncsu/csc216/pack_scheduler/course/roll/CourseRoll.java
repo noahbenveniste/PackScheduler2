7b26abc05269b7c13a35c264919c040e66828592
noahbenveniste <benven.noa1995@gmail.com>
2017-10-19 10:30:49 -0400

    Finished implementing CourseRoll
    unit test status: untested

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/roll/CourseRoll.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/roll/CourseRoll.java
@@ -44,10 +44,80 @@ public class CourseRoll {
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * Getter for the current enrollment capacity
+	 * @return the enrollment capacity
 	 */
 	public int getEnrollmentCap() {
 		return this.enrollmentCap;
+	}
+	
+	/**
+	 * Getter for the number of open seats left in the course roll
+	 * @return the number of open seats
+	 */
+	public int getOpenSeats() {
+		return this.enrollmentCap - this.roll.size();
+	}
+	
+	/**
+	 * Checks if a student is able to be added to a course roll
+	 * @return true if they are, false if there is no space left or they are already enrolled
+	 */
+	public boolean canEnroll(Student s) {
+		//Check that the list isn't full
+		if (this.roll.size() == this.getEnrollmentCap()) {
+			return false;
+		}
+		//Check for duplicates
+		for (int i = 0; i < this.roll.size(); i++) {
+			if (s.equals(this.roll.get(i))) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	/**
+	 * Adds a student to the course roll
+	 * @param s the student to be added
+	 * @throws IllegalArgumentException if the student is null, there is no more room in the
+	 * course roll, the student is already enrolled, or attempting to add the student to
+	 * the list otherwise throws an exception
+	 */
+	public void enroll(Student s) {
+		if (this.canEnroll(s)) {
+			try {
+				this.roll.add(this.roll.size(), s);
+			} catch (NullPointerException e) {
+				throw new IllegalArgumentException(e.getMessage());
+			} catch (IndexOutOfBoundsException e) {
+				throw new IllegalArgumentException(e.getMessage());
+			} catch (IllegalArgumentException e) {
+				throw new IllegalArgumentException(e.getMessage());
+			}
+		} else {
+			throw new IllegalArgumentException("The student could not be added because either because the course roll is full"
+					+ "or because the student is already enrolled.");
+		}
+	}
+	
+	/**
+	 * Drops the specified student from the course roll
+	 * @throws IllegalArgumentException if the student is null or removing the student from the list otherwise
+	 * generates an exception
+	 */
+	public void drop(Student s) {
+		if (s == null) {
+			throw new IllegalArgumentException("Student to remove cannot be null.");
+		}
+		for (int i = 0; i < this.roll.size(); i++) {
+			if (s.equals(this.roll.get(i))) {
+				try {
+					this.roll.remove(i);
+				} catch (IndexOutOfBoundsException e) {
+					throw new IllegalArgumentException(e.getMessage());
+				}
+			}
+		}
 	}
 }
