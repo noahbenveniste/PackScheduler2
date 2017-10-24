bf5da927d9798c75ff0a87a7625407cf9ccd7804
Kwhildne <kwhildne@ncsu.edu>
2017-10-23 22:09:50 -0400

    Fixed CheckStyle errors in CourseRoll.java

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/roll/CourseRoll.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/course/roll/CourseRoll.java
@@ -18,60 +18,80 @@ public class CourseRoll {
 	private static final int MIN_ENROLLMENT = 10;
 	/** The maximum number of students that a CourseRoll can enroll */
 	private static final int MAX_ENROLLMENT = 250;
-	
+
 	/**
-	 * Creates a new course roll. 
+	 * Creates a new course roll.
 	 * 
-	 * @param 
+	 * @param enrollmentCap
+	 *            the maximum number of students that will be allowed to enroll in
+	 *            the course. Can be from 10-250 inclusive.
+	 * @throws IllegalArgumentException
+	 *             if the enrollment cap input is below ten, above 250, blank or
+	 *             null
 	 */
 	public CourseRoll(int enrollmentCap) {
 		try {
 			this.setEnrollmentCap(enrollmentCap);
-		} catch (IllegalArgumentException e) { 
+		} catch (IllegalArgumentException e) {
 			throw new IllegalArgumentException(e.getMessage());
 		}
 		roll = new LinkedAbstractList<Student>(this.getEnrollmentCap());
 	}
-	
+
 	/**
+	 * Sets the maximum number of students that are allowed to enroll in a course
 	 * 
+	 * @param enrollmentCap
+	 *            the new maximum number of students that are allowed to enroll in
+	 *            the course
+	 * @throws IllegalArgumentException
+	 *             if enrollementCap is less than 10, greater than 250, blank, null,
+	 *             or less than the number of students who are already enrolled in
+	 *             the course.
 	 */
 	public void setEnrollmentCap(int enrollmentCap) {
 		if (enrollmentCap < MIN_ENROLLMENT || enrollmentCap > MAX_ENROLLMENT) {
 			throw new IllegalArgumentException("Invalid enrollment capacity.");
 		}
 		if (roll != null && (enrollmentCap < this.roll.size())) {
-			throw new IllegalArgumentException("New enrollment capacity cannot be less than the number of currently enrolled students.");
+			throw new IllegalArgumentException(
+					"New enrollment capacity cannot be less than the number of currently enrolled students.");
 		}
 		this.enrollmentCap = enrollmentCap;
 	}
-	
+
 	/**
 	 * Getter for the current enrollment capacity
+	 * 
 	 * @return the enrollment capacity
 	 */
 	public int getEnrollmentCap() {
 		return this.enrollmentCap;
 	}
-	
+
 	/**
 	 * Getter for the number of open seats left in the course roll
+	 * 
 	 * @return the number of open seats
 	 */
 	public int getOpenSeats() {
 		return this.enrollmentCap - this.roll.size();
 	}
-	
+
 	/**
 	 * Checks if a student is able to be added to a course roll
-	 * @return true if they are, false if there is no space left or they are already enrolled
+	 * 
+	 * @param s
+	 *            the student who's ability to be enrolled is being checked.
+	 * @return true if they are, false if there is no space left or they are already
+	 *         enrolled
 	 */
 	public boolean canEnroll(Student s) {
-		//Check that the list isn't full
+		// Check that the list isn't full
 		if (this.roll.size() == this.getEnrollmentCap()) {
 			return false;
 		}
-		//Check for duplicates
+		// Check for duplicates
 		for (int i = 0; i < this.roll.size(); i++) {
 			if (this.roll.get(i).equals(s)) {
 				return false;
@@ -79,13 +99,16 @@ public class CourseRoll {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Adds a student to the course roll
-	 * @param s the student to be added
-	 * @throws IllegalArgumentException if the student is null, there is no more room in the
-	 * course roll, the student is already enrolled, or attempting to add the student to
-	 * the list otherwise throws an exception
+	 * 
+	 * @param s
+	 *            the student to be added
+	 * @throws IllegalArgumentException
+	 *             if the student is null, there is no more room in the course roll,
+	 *             the student is already enrolled, or attempting to add the student
+	 *             to the list otherwise throws an exception
 	 */
 	public void enroll(Student s) {
 		if (this.canEnroll(s)) {
@@ -99,15 +122,20 @@ public class CourseRoll {
 				throw new IllegalArgumentException(e.getMessage());
 			}
 		} else {
-			throw new IllegalArgumentException("The student could not be added because either because the course roll is full"
-					+ " or because the student is already enrolled.");
+			throw new IllegalArgumentException(
+					"The student could not be added because either because the course roll is full"
+							+ " or because the student is already enrolled.");
 		}
 	}
-	
+
 	/**
 	 * Drops the specified student from the course roll
-	 * @throws IllegalArgumentException if the student is null or removing the student from the list otherwise
-	 * generates an exception
+	 * 
+	 * @param s
+	 *            the student who is dropping from the course
+	 * @throws IllegalArgumentException
+	 *             if the student is null or removing the student from the list
+	 *             otherwise generates an exception
 	 */
 	public void drop(Student s) {
 		if (s == null) {
